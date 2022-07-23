96d54b1c27ea8674a31ec616293b9dc94ec14d59
shsotoudegan <shsotoudegan@gmail.com>
2022-07-24 00:26:17 +0430

    Add a func

--- dbdriver.py
+++ dbdriver.py
@@ -1,10 +1,22 @@
 import sqlite3
+
 from pathlib import Path
 
 
 class DBdriver():
-    DIR = Path(__file__).resolve()
 
-    def __init__(self, dir: str, name: str):
-        self.dbpath = dir
+    __DIR = Path(__file__).resolve()
+
+    def __init__(self, name: str, dir=__DIR.parent):
         self.dbname = name
+        self.dbpath = dir.__str__() + self.dbname + '.db'
+        self.conection = sqlite3.connect(self.dbpath)
+        self.cursor = self.conection.cursor()
+
+    def create_table(self, name: str, *columns):
+        string = ''
+        for column in columns:
+            string = string + column
+        print(string)
+        self.cursor.execute(f"CREATE TABLE {name} ({string});")
+        self.conection.commit()
