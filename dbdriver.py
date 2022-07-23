f526c930729e74f6f7430083fa010a3cc9019eed
shsotoudegan <shsotoudegan@gmail.com>
2022-07-24 02:03:27 +0430

    Add and Complete insert and remove methods

--- dbdriver.py
+++ dbdriver.py
@@ -10,8 +10,9 @@ class DBdriver():
     def __init__(self, name: str, dir=__DIR.parent):
         self.dbname = name
         self.dbpath = dir.__str__() + '/' + self.dbname + '.db'
+        self.__var_filler()
 
-    def __cncdec(func):
+    def __conection_manager(func):
         def wrapper(*args, **kwargs):
             self = args[0]
             self.conection = sqlite3.connect(self.dbpath)
@@ -22,9 +23,34 @@ class DBdriver():
             return result
         return wrapper
 
-    @__cncdec
+    @__conection_manager
+    def __var_filler(self):
+        self.cursor.execute(
+            'SELECT name from sqlite_master where type= "table"')
+        self.tables = self.cursor.fetchall()
+
+    @__conection_manager
     def create_table(self, name: str, *columns):
+        self.tables.append(name)
         string = ''
         for column in columns:
-            string = string + column + ' '
+            string = string + column + ', '
+        string = string[:-2]
+        print(string)
         self.cursor.execute(f"CREATE TABLE {name} ({string});")
+
+    @__conection_manager
+    def insert(self, table: str, values: tuple):
+        string = ''
+        for value in values:
+            if type(value) == str:
+                string = string + "'" + value + "', "
+            else:
+                string = string + value.__str__() + ", "
+        string = string[:-2]
+        print(string)
+        self.cursor.execute(f"INSERT INTO {table} VALUES ({string});")
+
+    @__conection_manager
+    def remove(self, table, rowid: int):
+        self.cursor.execute(f"DELETE from {table} WHERE rowid = {rowid};")
